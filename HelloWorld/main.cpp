0c5f737eba8d11fa7f67e6040fc455b2ce98297c
汪点点 <diandian@wangdiandiandeMacBook-Pro.local>
2018-11-03 15:56:50 +0800

    first commit

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -7,9 +7,11 @@
 //
 
 #include <iostream>
+#include <stdio.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    printf("Hello world with prinft\n");
     return 0;
 }
