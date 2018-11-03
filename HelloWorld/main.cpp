6f6dfd00b443911ba48a4383da1250c9b374912a
汪丽君 <diandian@wangdiandiandemacbook-pro.local>
2018-11-03 14:51:05 +0800

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by 汪点点 on 2018/11/3.
+//  Copyright © 2018 ljwang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
